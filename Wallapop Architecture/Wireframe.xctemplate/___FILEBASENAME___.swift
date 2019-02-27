41af2aea3cb5e07c7fc15a4114ad5e3d1e576ad9
Aitor Pagán <aitor.pagan@wallapop.com>
2019-02-27 17:29:06 +0100

    Created Wireframe template

---
+++ Wallapop Architecture/Wireframe.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,20 @@
+import Foundation
+import Common
+import CommonUI
+import Domain
+
+protocol ___VARIABLE_wireframeName___Wireframing {
+    <#add wireframe methods#>
+}
+
+final class ___VARIABLE_wireframeName___Wireframe {
+    <#add parameters#>
+    
+    init(<#parameters#>) {
+        <#statements#>
+    }
+}
+
+extension ___VARIABLE_wireframeName___Wireframe: ___VARIABLE_wireframeName___Wireframing {
+    <#add wireframe methods#>
+}
