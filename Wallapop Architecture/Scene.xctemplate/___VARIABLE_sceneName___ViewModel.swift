6ade1b6d5ce917c8019d31a44258ac74937bd38e
Josep Escobar <josep.escobar@wallapop.com>
2020-12-31 09:54:51 +0100

    Modified viewmodel template implementation

--- Wallapop Architecture/Scene.xctemplate/___VARIABLE_sceneName___ViewModel.swift
+++ Wallapop Architecture/Scene.xctemplate/___VARIABLE_sceneName___ViewModel.swift
@@ -1,11 +1,9 @@
 struct ___VARIABLE_sceneName___ViewModel {
-    let titleText: String
-    let subtitleText: String
+    let templateVar: String
 }
 
 extension ___VARIABLE_sceneName___ViewModel {
-    init(localize: @escaping (String) -> String = { Localize($0) }) {
-        self.titleText = localize("TEMPLATE TITLE")
-        self.subtitleText = localize("TEMPLATE SUBTITLE")
+    init(localize: @escaping (String) -> String = dependencies.legacy.localize/* in main app use -> { Localize($0) } */) {
+        self.templateVar = localize("TEMPLATE VALUE")
     }
 }
